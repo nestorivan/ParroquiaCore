ba32d2ead384aae7145ef52408c341dec2c4d752
nestorivan <nestor.lopez.delgadillo@gmail.com>
2016-12-06 01:32:08 -0600

    Removing Files from source control

--- src/ParroquiaCore/ts/home.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var homeCtrl = (function () {
-    function homeCtrl() {
-        this.title = 'hello from home controller';
-    }
-    return homeCtrl;
-}());
-exports.homeCtrl = homeCtrl;
